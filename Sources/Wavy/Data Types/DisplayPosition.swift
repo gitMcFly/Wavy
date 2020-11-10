3d2af7db28e8bda7e0a6c27d4eeb651b26a283a7
Christopher Weems <hello@christopherweems.com>
2020-11-10 15:24:04 -0500

    Add `DisplayPosition`

---
+++ Sources/Wavy/Data Types/DisplayPosition.swift
@@ -0,0 +1,13 @@
+//
+//  DisplayPosition.swift
+//  
+//
+//  Created by Christopher Weems on 11/10/20.
+//
+
+public enum DisplayPosition {
+    case preceeding
+    case following
+    case hidden
+    
+}
