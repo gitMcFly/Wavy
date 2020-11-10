0fc6c6d16ca26fc9eaaad1a756b8df48b64546f8
Christopher Weems <hello@christopherweems.com>
2020-11-09 20:43:34 -0500

    Add `SetResult` result builder

---
+++ Sources/Wavy/Result Builders/SetResult.swift
@@ -0,0 +1,33 @@
+//
+//  SetResult.swift
+//  
+//
+//  Created by Christopher Weems on 11/9/20.
+//
+
+import Foundation
+
+@_functionBuilder
+public struct SetResult {
+    public static func buildBlock<E>() -> Set<E> {
+        Set<E>()
+    }
+    
+    public static func buildBlock<E>(_ subsets: Set<E>...) -> Set<E> {
+        var combined = Set<E>()
+        subsets.forEach { combined.formUnion($0) }
+        return combined
+    }
+    
+}
+
+public extension SetResult {
+    static func buildExpression<E>(_ subset: Set<E>) -> Set<E> {
+        subset
+    }
+    
+    static func buildExpression<E>(_ element: E) -> Set<E> {
+        Set([element])
+    }
+    
+}
