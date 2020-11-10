0e8d52ed44dd05711e987e20b8a7f163f4eeedd4
Christopher Weems <hello@christopherweems.com>
2020-11-10 18:41:58 -0500

    Add `Broadcaster`

---
+++ Sources/Wavy/Broadcaster.swift
@@ -0,0 +1,15 @@
+//
+//  Broadcaster.swift
+//  
+//
+//  Created by Christopher Weems on 11/10/20.
+//
+
+import Foundation
+
+public enum Broadcaster {
+    case cumulusMedia
+    case midwestCommunications
+    case salemMediaGroup
+    
+}
