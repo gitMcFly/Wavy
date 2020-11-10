03ae1bb8d6d9d6471d73c0058c64656219b2183d
Christopher Weems <hello@christopherweems.com>
2020-11-10 17:21:34 -0500

    Add Atlanta radio stations

--- Sources/Stations/Georgia/Atlanta.swift
+++ Sources/Stations/Georgia/Atlanta.swift
@@ -8,11 +8,78 @@
 extension Georgia {
     struct Atlanta: _StationGroup {
         var stations: Set<Station> {
+            
+            // MARK: - FM Stations
+            
+            Station("wras", "Album 88", .fm(88, 5))
+                .during(timeFrom: (5, 00), to: (19, 00), in: .atlanta) {
+                    $0.title("88.5 GPB")
+                    
+                }
+                .ignoreFrequencyInTitle()
+                .url(authority: "wras.org")
+            
+            Station("wrfg", .fm(89, 3))
+                .url(authority: "wrfg.org")
+            
+            Station("wabe", .fm(90, 1))
+                .callLettersFollowFrequency()
+                .url(authority: "wabe.org")
+            
+            Station("wrek", .fm(91, 1))
+                .url(authority: "wrek.org")
+            
+            Station("wclk", .fm(91, 9))
+                .url(authority: "wclk.com")
+            
+            Station("wzgc", .fm(92, 9), "The Game")
+                .frequencyDesignator(position: .hidden)
+                .url(authority: "929thegame.radio.com")
+            
+            Station("wubl", .fm(94, 9), "The Bull")
+                .slogan("Today's Best Country")
+                .url(authority: "949thebull.iheart.com")
+            
+            Station("wsb", .fm(95, 5))
+                .slogan("Atlanta's News & Talk")
+                .url(authority: "wsbradio.com")
+            
+            Station("wwpv", "Power", .fm(96, 1))
+                .url(authority: "power961.iheart.com")
+                .frequencyDesignator(position: .hidden)
+            
             Station("WSB-FM", "B-", .fm(98, 5))
                 .url(authority: "b985.com")
             
+            Station("wwwq", "Q-", .fm(99, 1))
+                .url(authority: "q997atlanta.com")
+            
+            Station("wvee", "V103", .fm(103, 3))
+                .url(authority: "v103.radio.com")
+                .ignoreFrequencyInTitle()
+            
+            Station("wifn", "ESPN Atlanta", .fm(103, 7))
+                .ignoreBroadcastCityInTitle()
+            
+            
+            // MARK: - AM Stations
+            
+            Station("wbin", "Atlanta's BIN", .am(640))
+                .url(authority: "atlanta.binbews.com")
+                .ignoreBroadcastCityInTitle()
+                .frequencyDesignator(position: .hidden)
+            
+            Station("wqxi", "Atlanta Radio Korea", .am(790))
+                .ignoreBroadcastCityInTitle()
+                .url(authority: "atlantaradiokorea.com")
+            
             Station("waec", "Love", .am(860))
                 .url(authority: "love860.com")
+            
+            Station("wgka", .am(920), "The Answer")
+                .slogan("News. Opinion. Insight.")
+                .url(authority: "am920theanswer.com")
+                .frequencyDesignator(position: .preceeding)
             
             Station("wniv", "Faith Talk", .am(970))
                 .url(authority: "faithtalk970.com")
@@ -21,6 +88,15 @@ extension Georgia {
                 //.url(authority: "wtza.net")
             
             Station("wafs", "Relevant Radio", .am(1190))
+            
+            Station("waok", "News & Talk", .am(1380))
+                .url(authority: "waok.radio.com")
+                .frequencyDesignator(position: .hidden)
+                .callLettersFollowFrequency()
+            
+            Station("wyze", .am(1480))
+                .slogan("A Voice Changing The World")
+                .url(authority: "wyzeradio.com")
             
         }
         
