e623d40304db4d952e07ce127ac07adc36968809
Christopher Weems <hello@christopherweems.com>
2020-11-10 18:42:07 -0500

    Add `Network`

---
+++ Sources/Wavy/Network.swift
@@ -0,0 +1,33 @@
+//
+//  Network.swift
+//  
+//
+//  Created by Christopher Weems on 11/10/20.
+//
+
+import Foundation
+
+public enum Network: Hashable {
+    
+    // music
+    case nashFM
+
+    // news
+    case americanUrbanRadio
+    case associatedPress
+    case blackInformation
+    case bloomberg
+    case foxNews
+    case npr
+    
+    // religious
+    case americanFamilyRadio
+    case bottRadio
+    case moodyRadio
+    case wayFM
+    
+    // sports
+    case espnRadio
+    case foxSports
+    
+}
