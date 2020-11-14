911a888df837048bc7ef570104682b88cb9fe5be
Christopher Weems <hello@christopherweems.com>
2020-11-14 16:18:02 -0500

    Add new Huntsville stations

--- Sources/Stations/Alabama/Huntsville.swift
+++ Sources/Stations/Alabama/Huntsville.swift
@@ -8,11 +8,24 @@
 extension Alabama {
     struct Huntsville: _StationGroup {
         var stations: Set<Station> {
+            Station("wayh", .fm(88, 1))
+                .member(of: .wayFM)
+            
+            Station("wlrh", .fm(89, 3))
+                .member(of: .apm, .prx, .npr)
+                .url(authority: "wlrh.org")
+            
+            Station("wjou", "Praise", .fm(90, 1))
+                .url(authority: "wjou.org")
+            
             Station("wjab", .fm(90, 9))
                 .url(authority: "wjab.org")
             
-            Station("wlrh", .fm(89, 3))
-                .url(authority: "wlrh.org")
+            Station("wfyd", .fm(91, 7))
+                .owner(.bibleBroadcastingNetwork)
+            
+            Station("weuz", .fm(92, 1))
+                .url(authority: "103weup.com")
             
         }
     }
