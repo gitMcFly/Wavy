8448e094375281e1ad05a6bbbae24b251ae1f7f5
Christopher Weems <hello@christopherweems.com>
2020-11-18 11:41:58 -0500

    Add new stations

--- Sources/Stations/California/LosAngeles.swift
+++ Sources/Stations/California/LosAngeles.swift
@@ -10,9 +10,26 @@ import Foundation
 extension California {
     struct LosAngeles: _StationGroup {
         var stations: Set<Station> {
+            
+            // fm stations
+            
+            Station("klve", .fm(107, 5), "K-LOVE")
+                .slogan("Vive Tu Música")
+                .url("https://www.univision.com/los-angeles/klve/")
+                .owner(.univision)
+            
+            Station("kwve-fm", "K-Wave", .fm(107, 9))
+                .slogan("The Wave of Living Water")
+                .url(authority: "kwve.com")
+            
+            
+            // am stations
+            
             Station("knx", .am(1070), "Newsradio")
-                .callLetterPosition(.leading)
+                .callLetters(.leading)
                 .frequencyDesignator(.hidden)
+                .slogan("All News, All the Time. Depend on Us.")
+                .member(of: .cbsNewsRadio, .bloomberg, .theWeatherChannel)
                 .url(authority: "knx1070.radio.com")
                 .owner(.entercom)
             
