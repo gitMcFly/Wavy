34e7ec0e230cd764b19fffe100709223148c000f
Christopher Weems <hello@christopherweems.com>
2020-11-30 21:41:15 -0500

    Add `AnyShow.Properties.network`

--- Sources/ShowGroup/AnyShow.swift
+++ Sources/ShowGroup/AnyShow.swift
@@ -5,9 +5,12 @@
 //  Created by Christopher Weems on 11/30/20.
 //
 
+import StationGroup
+
 public struct AnyShow: Show, ExpressibleByStringLiteral {
     public struct Properties {
         var title: String?
+        var network: Network?
         
     }
     
