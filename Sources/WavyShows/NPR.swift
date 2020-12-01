eceff96b8ebfad7b3d60d32f1314ea5e1a456e21
Christopher Weems <hello@christopherweems.com>
2020-12-01 10:04:59 -0500

    Add new stations

--- Sources/WavyShows/NPR.swift
+++ Sources/WavyShows/NPR.swift
@@ -13,7 +13,26 @@ public struct NPR: ShowGroup {
     public static let network = Network.npr
     
     public var body: AnyShowGroup {
+        "Morning Edition"
+            .url("https://www.npr.org/programs/morning-edition/")
+        
+        "Weekend Edition Saturday"
+            .url("https://www.npr.org/programs/weekend-edition-saturday/")
+        
+        "Weekend Edition Sunday"
+            .url("https://www.npr.org/programs/weekend-edition-sunday/")
+        
         "All Things Considered"
+            .url("https://www.npr.org/programs/all-things-considered/")
+        
+        "Fresh Air"
+            .url("https://www.npr.org/programs/fresh-air/")
+        
+        "1A"
+            .url("https://the1a.org")
+        
+        "Here & Now"
+            .url("https://www.wbur.org/hereandnow")
         
     }
     
