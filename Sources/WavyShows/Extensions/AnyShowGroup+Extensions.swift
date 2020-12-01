cd121f328a931e8d64d167abc7214000250b4bf3
Christopher Weems <hello@christopherweems.com>
2020-12-01 17:20:13 -0500

    Add `AnyShowGroup.universal`

---
+++ Sources/WavyShows/Extensions/AnyShowGroup+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  AnyShowGroup.swift
+//  
+//
+//  Created by Christopher Weems on 12/1/20.
+//
+
+import ShowGroup
+
+public extension AnyShowGroup {
+    @AnyShowGroupBuilder static var universal: AnyShowGroup {
+        NPR()
+        PRX()
+        
+    }
+    
+}
+
