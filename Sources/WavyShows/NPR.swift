c5084323084c29f55f173532f6fabdb56efdc7df
Christopher Weems <hello@christopherweems.com>
2020-12-01 18:27:30 -0500

    [gardening]

--- Sources/WavyShows/NPR.swift
+++ Sources/WavyShows/NPR.swift
@@ -5,9 +5,7 @@
 //  Created by Christopher Weems on 11/30/20.
 //
 
-import Foundation
 import ShowGroup
-import unstandard
 
 public struct NPR: ShowGroup {
     public static let network = Network.npr
