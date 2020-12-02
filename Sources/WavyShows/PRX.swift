6c351771a451b44f2559507ea88e06cb4d8ac028
Christopher Weems <hello@christopherweems.com>
2020-12-01 20:06:06 -0500

    Add `PRX` shows

--- Sources/WavyShows/PRX.swift
+++ Sources/WavyShows/PRX.swift
@@ -11,7 +11,33 @@ public struct PRX: ShowGroup {
     public static let network = Network.prx
     
     public var showGroupBody: some ShowGroup {
-        EmptyShowGroup()
+        "Afropop Worldwide"
+            .url(authority: "afropop.org")
+        
+        "This American Life"
+            .url(authority: "thisamericanlife.org")
+        
+        "Jazz After Hours"
+            .url(authority: "jazzafterhours.net")
+        
+        "Latino USA"
+            .url(authority: "latinousa.org")
+        
+        "Live Wire"
+            .subtitle("with Luke Burbank")
+            .url(authority: "livewireradio.org")
+        
+        "The Moth"
+            .url(authority: "themoth.org")
+        
+        "Reveal"
+            .url(authority: "revealnews.org")
+        
+        "Snap Judgement"
+            .url(authority: "snapjudgment.org")
+        
+        "The World"
+            .url(authority: "pri.org")
         
     }
     
