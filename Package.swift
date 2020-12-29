3ff03c026f8441cc160f27106714ed42ac9307bd
Christopher Weems <hello@christopherweems>
2020-12-29 03:03:52 -0500

    Update minimum `Statehood` package version to "0.0.2"

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-algorithms", .upToNextMajor(from: "0.0.1")),
         .package(url: "https://github.com/gitMcFly/MixedGroup", .upToNextMajor(from: "0.0.1")),
         .package(url: "https://github.com/christopherweems/Resultto", .upToNextMajor(from: "0.0.2")),
-        .package(url: "https://github.com/christopherweems/Statehood", .upToNextMajor(from: "0.0.1")),
+        .package(url: "https://github.com/christopherweems/Statehood", .upToNextMajor(from: "0.0.2")),
         .package(url: "https://github.com/christopherweems/unstandard", .upToNextMajor(from: "0.0.6")),
     ],
     targets: [
