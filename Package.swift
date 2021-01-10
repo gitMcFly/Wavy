685da163f43688452c6e6ea43554141b5214779d
Christopher Weems <hello@christopherweems>
2021-01-10 17:14:08 -0500

    Update package dependencies

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         .package(url: "https://github.com/gitMcFly/MixedGroup", .upToNextMajor(from: "0.0.1")),
         .package(url: "https://github.com/christopherweems/Resultto", .upToNextMajor(from: "0.0.2")),
         .package(url: "https://github.com/christopherweems/Statehood", .upToNextMajor(from: "0.0.2")),
-        .package(url: "https://github.com/christopherweems/unstandard", .upToNextMajor(from: "0.0.6")),
+        .package(url: "https://github.com/christopherweems/unstandard", .upToNextMajor(from: "0.0.15")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
